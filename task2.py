27b11e51c059215f737f8acbe00d129b0bfb18dd
DimonYarkin <dmitryyarkin@yandex.ru>
2020-09-16 18:01:26 +0300

    изменено задание 2

--- task2.py
+++ task2.py
@@ -4,4 +4,3 @@ my_list = [int(random() * 300) for el in range(20)]
 print(f"Исходный список {my_list}")
 list_new = [my_list[i] for i in range(1, len(my_list)) if my_list[i - 1] < my_list[i]]
 print(f"Отобранный список: {list_new}")
-print(f'Числа от 20 до 240 кратные 20 или 21 - {[el for el in range(20, 241) if el % 20 == 0 or el % 21 == 0]}')
