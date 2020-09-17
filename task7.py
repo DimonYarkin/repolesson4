614fca8f8c118c49203e1bb07216dcd19639c344
DimonYarkin <dmitryyarkin@yandex.ru>
2020-09-17 16:30:48 +0300

    Выполнено задание 7 изменено

--- task7.py
+++ task7.py
@@ -5,13 +5,20 @@ from math import factorial
 def fibo_gen():
     for el in count(1):
         yield factorial(el)
+while True:
 
-
-n = int(input("Введите количесво чисел для вывода: "))
-numStop = 0
-for i in fibo_gen():
-    if numStop < n:
-        print(i)
-        numStop += 1
-    else:
+    n = 0
+    try:
+        n = int(input("Введите количесво чисел для вывода: "))
+    except ValueError:
+        print("Ошибка")
         break
+
+    numStop = 0
+    for i in fibo_gen():
+        if numStop < n:
+            print(i)
+            numStop += 1
+        else:
+            break
+    break
